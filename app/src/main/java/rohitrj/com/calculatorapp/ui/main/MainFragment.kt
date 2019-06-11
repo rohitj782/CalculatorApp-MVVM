5246e7362b0cd23e7b8fd251c5619c323c3d614f
rohitj782 <rohitj782@gmail.com>
2019-06-11 12:25:27 +0530

    Implementing button clicks

--- app/src/main/java/rohitrj/com/calculatorapp/ui/main/MainFragment.kt
+++ app/src/main/java/rohitrj/com/calculatorapp/ui/main/MainFragment.kt
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main_fragment.*
 import rohitrj.com.calculatorapp.R
+import kotlin.math.min
 
 class MainFragment : Fragment() {
 
@@ -26,7 +29,103 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        textViewResult.text = viewModel.getMessage()
+
+        lateinit var value: String
+
+        //retrieving values when any  button is clicked
+        one.setOnClickListener {
+            value = one.text.toString()
+            performOperation(value)
+        }
+        two.setOnClickListener {
+            value = two.text.toString()
+            performOperation(value)
+        }
+        three.setOnClickListener {
+            value = three.text.toString()
+            performOperation(value)
+        }
+        four.setOnClickListener {
+            value = four.text.toString()
+            performOperation(value)
+        }
+        five.setOnClickListener {
+            value = five.text.toString()
+            performOperation(value)
+        }
+        six.setOnClickListener {
+            value = six.text.toString()
+            performOperation(value)
+        }
+        seven.setOnClickListener {
+            value = seven.text.toString()
+            performOperation(value)
+        }
+        eight.setOnClickListener {
+            value = eight.text.toString()
+            performOperation(value)
+        }
+        nine.setOnClickListener {
+            value = nine.text.toString()
+            performOperation(value)
+        }
+        zero.setOnClickListener {
+            value = zero.text.toString()
+            performOperation(value)
+        }
+
+        clear.setOnClickListener {
+            value = clear.text.toString()
+            performOperation(value)
+        }
+        left.setOnClickListener {
+            value = left.text.toString()
+            performOperation(value)
+        }
+        right.setOnClickListener {
+            value = right.text.toString()
+            performOperation(value)
+        }
+        delete.setOnClickListener {
+            value = delete.text.toString()
+            performOperation(value)
+        }
+        plus.setOnClickListener {
+            value = plus.text.toString()
+            performOperation(value)
+        }
+        minus.setOnClickListener {
+            value = minus.text.toString()
+            performOperation(value)
+        }
+
+        multiply.setOnClickListener {
+            value = multiply.text.toString()
+            performOperation(value)
+        }
+
+        divide.setOnClickListener {
+            value = divide.text.toString()
+            performOperation(value)
+        }
+        decimal.setOnClickListener {
+            value = decimal.text.toString()
+            performOperation(value)
+        }
+
+        equals.setOnClickListener {
+            value = equals.text.toString()
+            performOperation(value)
+        }
+
+
+    }
+
+    fun performOperation(value: String) {
+
+        Toast.makeText(context, value+"", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/rohitrj/com/calculatorapp/ui/main/MainViewModel.kt
+++ app/src/main/java/rohitrj/com/calculatorapp/ui/main/MainViewModel.kt
@@ -4,4 +4,14 @@ import android.arch.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    var myMessage:String?=null
+
+    fun setMessage(s:String?){
+        myMessage=s?:"Null Value"
+    }
+
+    fun getMessage(): String? {
+        return myMessage?:"Null Value"
+    }
+
 }
